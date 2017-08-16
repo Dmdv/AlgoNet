a20004c7deca268f0121128ea4f723f20f4d5bda
Dmitry Dyachkov <ddyachkov@luxoft.com>
2017-08-16 03:11:43 +0300

    Added AllSubsets function

--- AlgorithmsNet/Algorithms/Combinatorics/EnumerableExtensions.cs
+++ AlgorithmsNet/Algorithms/Combinatorics/EnumerableExtensions.cs
@@ -6,13 +6,53 @@ namespace AlgoNet.Algorithms.Combinatorics
 {
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Gets combinations from set by count
+        /// </summary>
         [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
-        public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> elements, int k)
+        public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> set, int count)
         {
-            return k == 0
+            return count == 0
                 ? new[] {new T[0]}
-                : elements.SelectMany((e, i) =>
-                    elements.Skip(i + 1).Combinations(k - 1).Select(c => new[] {e}.Concat(c)));
+                : set.SelectMany((e, i) =>
+                    set.Skip(i + 1).Combinations(count - 1).Select(c => new[] {e}.Concat(c)));
+        }
+
+        // Построение решения Р(n) для общего случая.
+        // Вычисляем Р(n-1), клонируем результаты и добавляем an в каждый из эти клонированных множеств
+
+        /// <summary>
+        /// Gets all subsets from the given set starting from index in set
+        /// </summary>
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        public static IEnumerable<IEnumerable<T>> AllSubsets<T>(this IEnumerable<T> set, int index = 0)
+        {
+            IEnumerable<IEnumerable<T>> allSubsets;
+
+            if (set.Count() == index)
+            {
+                allSubsets = new List<IEnumerable<T>> {new List<T>()}; // добавить пустое множество в базовом случае
+            }
+            else
+            {
+                allSubsets = AllSubsets(set, index + 1).ToArray();
+
+                var item = set.ElementAt(index);
+
+                var moreSubsets = new List<IEnumerable<T>>();
+
+                foreach (var subset in allSubsets)
+                {
+                    var newSet = new List<T>();
+                    newSet.AddRange(subset);
+                    newSet.Add(item);
+                    moreSubsets.Add(newSet);
+                }
+
+                allSubsets = allSubsets.Concat(moreSubsets);
+            }
+
+            return allSubsets;
         }
     }
 }
--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -1,23 +1,31 @@
-﻿using System;
-using System.Linq;
-using AlgoNet.Algorithms.Combinatorics;
+﻿using AlgoNet.Algorithms.Combinatorics;
 using AlgoNet.IO;
+using System;
+using System.Linq;
 
 namespace ConsoleTests
 {
-    internal class Program
+    internal static class Program
     {
         public static void Main(string[] args)
         {
-            var array = new[] {1, 2, 3};
+            var array = new[] {1, 2, 3, 4};
 
             for (var i = 0; i <= array.Length; i++)
             {
                 foreach (var combination in array.Combinations(i))
                 {
                     var ouput = string.Join(",", combination.Select(x => x.ToString()));
-                    Console.WriteLine("({0})", ouput);
+                    Console.WriteLine($"({ouput})");
                 }
+            }
+
+            Console.WriteLine("---------- AllSubsets -----------");
+
+            foreach (var combination in array.AllSubsets())
+            {
+                var ouput = string.Join(",", combination.Select(x => x.ToString()));
+                Console.WriteLine($"({ouput})");
             }
 
             Console.ReadLine();
